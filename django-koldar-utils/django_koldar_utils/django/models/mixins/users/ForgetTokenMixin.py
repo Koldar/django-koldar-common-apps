435a0b5f9dc75fac7801ac7fd2b9aadca261303c
Massimo Bono <massimobono1@gmail.com>
2021-08-10 08:29:51 +0200

    create forget token mixin if you want your users to be able to receive forget password mails

--- django-koldar-utils/django_koldar_utils/django/models/mixins/users/ForgetTokenMixin.py
+++ django-koldar-utils/django_koldar_utils/django/models/mixins/users/ForgetTokenMixin.py
@@ -1,12 +1,15 @@
 from typing import Optional
 
 from arrow import Arrow
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from django.db import models
 
 from django_koldar_utils.django.Orm import Orm
+from django_koldar_utils.functions import secrets_helper
 
 
-class ForgeTokenMixin(models.Model):
+class ForgetTokenMixin(models.Model):
     """
     A mixin you should attach to the user object. This mixin add to the user the capability of a "forget password?".
     At database level, it adds 2 new columns: the forget token and the forget token creation time (used to check if
@@ -55,8 +58,59 @@ class ForgeTokenMixin(models.Model):
             return True
         return False
 
-    def send_forget_token_mail(self):
+    def try_reset_password(self, new_password: str, actual_forget_token: str):
         """
-        Send a mail to the email address provided during username registration with the expected token.
+        Reset the password by using a forget token sent via mail
         """
-        raise NotImplementedError()
+        # check if the there is a forget password token valid
+        if self.forget_password_token != actual_forget_token:
+            self._on_forget_token_invalid()
+        if not self.has_valid_forget_token:
+            self._on_forget_token_invalid()
+
+        # update password and clear the reset toke
+
+        salt = secrets_helper.get_random_alphanum_str(16)
+        self.password = make_password(new_password, salt=salt)
+        self.forget_password_token = None
+        self.forget_password_token_creation_date = None
+        self.save()
+
+    def _on_forget_token_invalid(self):
+        """
+        Method invoked if we detected an invalid forget token
+        """
+        raise ValueError(f"An invalid forget token has been detected! Token will expire at {self.forget_token_expiration_time}")
+
+    def _on_forget_token_already_present(self):
+        """
+        Method invoked if we have detected that a forget token is still valid
+        """
+        raise ValueError(f"A forget token has already been send. Token will expire at {self.forget_token_expiration_time}!")
+
+    @classmethod
+    def try_sending_forget_token_mail(cls, email: str):
+        """
+        Send to the email of this user a forget token mail.
+        If we cannot send a forget token, we will raise an exception
+
+        :param email: mail to send the forget token to. Might not be present in the system
+        """
+
+        UserModel = get_user_model()
+        user = UserModel.objects.find_only_or_fail(email=email)
+
+        # check if the there is a forget password token valid
+        if user.has_valid_forget_token:
+            user._on_forget_token_already_present()
+
+        # we need to refresh the forget token
+        user.forget_password_token = secrets_helper.get_random_alphanum_str(300)
+        user.forget_password_token_creation_date = Arrow.utcnow()
+        user.save()
+
+        # send email
+        user.email_user(
+            subject="Reset your email",
+            message=f"link is {user.forget_password_token}. The token will expire {user.forget_password_token_creation_date.isoformat()}. Thanks"
+        )
